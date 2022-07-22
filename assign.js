020d0174b0550e0ac099d050ca002d4c46e52799
JaneMusimbi <99467793+JaneMusimbi@users.noreply.github.com>
2022-07-22 09:36:06 +0300

    Update assign.js

--- assign.js
+++ assign.js
@@ -6,12 +6,12 @@ class Mkulima{
     this.addFarm = (farmId, name, farmer, phone, address) => {
     this.farms.push({farmId,name,farmer,phone,address})}
     this.removeFarm = (farmId) => {
-    let specificItem = this.farms.find(item => item.farmId ===farmId);
-    let farmIndex = this.farms.indexOf(specificItem);
+    var specificItem = this.farms.find(item => item.farmId ===farmId);
+    var farmIndex = this.farms.indexOf(specificItem);
     this.farms.splice(farmIndex, 1);
     }
     this.updateFarm = function(farmId,newFarmId,newFarmName,newFarmerName,newPhone,newAddress){
-    let specificFarm = this.farms.find(item => item.farmId ===farmId)
+    var specificFarm = this.farms.find(item => item.farmId ===farmId)
     specificFarm.farmId =newFarmId;
     specificFarm.farmName =newFarmName;
     specificFarm.farmer =newFarmerName;
@@ -23,12 +23,12 @@ class Mkulima{
     this.addProduct = (productId, productName, price) => {
     this.product.push({productId,productName,price})}
     this.removeProduct = (productId) => {
-    let specific = this.product.find(item => item.productId ===productId);
-    let productIndex = this.product.indexOf(specific);
+    var specific = this.product.find(item => item.productId ===productId);
+    var productIndex = this.product.indexOf(specific);
     this.product.splice(productIndex, 1);
     }
     this.updateProduct = function(productId,newProductId,newProductName,newPrice){
-    let specificProduct = this.product.find(item => item.productId ===productId)
+    var specificProduct = this.product.find(item => item.productId ===productId)
     specificProduct.productId =newProductId;
     specificProduct.productName = newProductName;
     specificProduct.price = newPrice;
@@ -38,11 +38,11 @@ class Mkulima{
     this.printProducts = ()=>{
     console.log(this.product)}
     this.calculateOrderCost = (productId,quantity)=>{
-    let specificProduct = this.product.find(object=>object.productId===productId);
+    Var specificProduct = this.product.find(object=>object.productId===productId);
     console.log(`${quantity} ${specificProduct.productName} for KES ${quantity*specificProduct.price}`);}
     }
     }
-    let farm1 = new Mkulima();
+    var farm1 = new Mkulima();
     farm1.addFarm("101","Jane","Musimbi","0712345678", "OB313");
     farm1.addFarm("235","Nora","Kadi","0123456757", "MC202");
     farm1.addFarm("132","JD","Winnie","073454320199", "BT305");
